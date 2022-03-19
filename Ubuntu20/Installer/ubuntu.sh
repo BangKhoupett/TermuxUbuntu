e3690855fa1b0e98896e0c9d59c22425914e68a2
BangKhoupett <bangkhoupett@gmail.com>
2022-03-19 22:06:32 +0700

    Update

--- Ubuntu20/Installer/ubuntu.sh
+++ Ubuntu20/Installer/ubuntu.sh
@@ -226,12 +226,12 @@ EOM
 mkdir -p ubuntu-fs/var/tmp
 rm -rf ubuntu-fs/usr/local/bin/*
 
-wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/.profile -O ubuntu-fs/root/.profile.1
+wget -q https://raw.githubusercontent.com/BangKhoupett/TermuxUbuntu/master/Ubuntu20/Rootfs/.profile -O ubuntu-fs/root/.profile.1
 cat $folder/root/.profile.1 >> $folder/root/.profile && rm -rf $folder/root/.profile.1
-wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vnc -P ubuntu-fs/usr/local/bin
-wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vncpasswd -P ubuntu-fs/usr/local/bin
-wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vncserver-stop -P ubuntu-fs/usr/local/bin
-wget -q https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Rootfs/Ubuntu19/vncserver-start -P ubuntu-fs/usr/local/bin
+wget -q https://raw.githubusercontent.com/BangKhoupett/TermuxUbuntu/master/Ubuntu20/Rootfs/vnc -P ubuntu-fs/usr/local/bin
+wget -q https://raw.githubusercontent.com/BangKhoupett/TermuxUbuntu/master/Ubuntu20/Rootfs/vncpasswd -P ubuntu-fs/usr/local/bin
+wget -q https://raw.githubusercontent.com/BangKhoupett/TermuxUbuntu/master/Ubuntu20/Rootfs/vncserver-stop -P ubuntu-fs/usr/local/bin
+wget -q https://raw.githubusercontent.com/BangKhoupett/TermuxUbuntu/master/Ubuntu20/Rootfs/vncserver-start -P ubuntu-fs/usr/local/bin
 
 chmod +x ubuntu-fs/root/.bash_profile
 chmod +x ubuntu-fs/root/.profile
